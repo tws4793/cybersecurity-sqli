8bad7f3fda2f5ab24ef09a9b6c0f444835aac5e7
Teo Wei Shen <tws4793@gmail.com>
2020-11-19 16:26:54 +0800

    Add function documentation

--- includes/common.php
+++ includes/common.php
@@ -12,6 +12,11 @@ if (!$connection) {
     die('Database Connection Failed: ' . mysqli_error($connection));
 }
 
+/**
+ * Unset the username and password, sets an error message and return to login page.
+ * 
+ * @param string $error error message to be displayed
+ */
 function loginFail($error)
 {
     unset($_POST['username'], $_POST['password']);
@@ -19,6 +24,20 @@ function loginFail($error)
     header('Location: .');
 }
 
+/**
+ * Get a specific user from the database.
+ * This function uses the insecure way of querying the database, and is subject to SQL Injection attacks.
+ * 
+ * Whereas a common query to the database will be in the form of:
+ *  SELECT * FROM accounts WHERE username='admin' AND password='password'
+ * An attacker can attempt to insert the string "' OR 1=1 --" such that the query will become:
+ *  SELECT * FROM accounts WHERE username='' OR 1=1-- AND password=''
+ * 
+ * @param string $username username of the user
+ * @param string $password password of the user
+ * @param mixed $connection an object which represents the connection to a MySQL Server
+ * @return array|null
+ */
 function getUser($username, $password, $connection)
 {
     $query = "SELECT * FROM accounts WHERE username='$username' AND password='$password'";
@@ -30,6 +49,18 @@ function getUser($username, $password, $connection)
     return $row;
 }
 
+/**
+ * Get a specific user from the database.
+ * This function uses the secure way of querying the database with prepared statements.
+ * 
+ * Any attempts by an attacker to insert the string "' OR 1=1 --" will result in failure,
+ * as the queries are parameterised.
+ * 
+ * @param string $username username of the user
+ * @param string $password password of the user
+ * @param mixed $connection an object which represents the connection to a MySQL Server
+ * @return array|null
+ */
 function getUserSecure($username, $password, $connection)
 {
     $query = "SELECT * FROM accounts WHERE username = ? AND password = ?";
@@ -55,6 +86,13 @@ function getUserSecure($username, $password, $connection)
     return $result;
 }
 
+/**
+ * Generate tables for presentation following an output from the SQL query.
+ * 
+ * @param array $labels key-value pair of row key and header
+ * @param mixed $sql_result an object from mysqli_query
+ * @return string
+ */
 function generateTables($labels, $sql_result)
 {
     $table_html = '<table class="table table-bordered text-white">';
@@ -80,6 +118,11 @@ function generateTables($labels, $sql_result)
     return $table_html;
 }
 
+/**
+ * Check whether user is logged in.
+ * 
+ * @return bool
+ */
 function isLoggedIn()
 {
     return isset($_SESSION['username'], $_SESSION['password']);
